497259b1da080a44d0281e3613accf8ff5feda4d
Andrii Ponomarov <andrii.ponomarov@gmail.com>
2021-07-25 15:33:09 -0400

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,8 +3,3 @@ require 'aruba/cucumber'
 Aruba.configure do |config|
   config.exit_timeout = 240
 end
-
-Before do
-  delete_environment_variable 'RUBYOPT'
-  delete_environment_variable 'BUNDLE_GEMFILE'
-end
